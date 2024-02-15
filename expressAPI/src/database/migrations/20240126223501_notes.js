2da01e4eabf51597ae3e91b731aa0408fdc5406a
Wendel Henrique (SouOWendel) <souowendel@gmail.com>
2024-02-15 18:05:41 -0300

    feat(notes migration): schema of notes was created with migrations of Knex

---
+++ expressAPI/src/database/migrations/20240126223501_notes.js
@@ -0,0 +1,34 @@
+/* eslint-disable jsdoc/require-jsdoc */
+/* eslint-disable require-jsdoc */
+/* eslint-disable jsdoc/require-param-description */
+
+// id, minCoreVersion, maxCoreVersion, minSysVersion, maxSysVersion, type,
+// display, title, content
+
+export function up(knex) {
+	return knex.schema.createTable('notes', function(table) {
+		table.increments('id').primary();
+		table.string('minCoreVersion');
+		table.string('maxCoreVersion');
+		table.string('minSysVersion').notNullable();
+		table.string('maxSysVersion').notNullable();
+		table.string('type').notNullable();
+		table.string('display').notNullable();
+		table.string('title').notNullable();
+		table.string('content').notNullable();
+		table.timestamp('createdAt').defaultTo(knex.fn.now());
+		table.timestamp('updatedAt').defaultTo(knex.fn.now());
+	}).then((result) => {
+		return result;
+	}).catch((err) => {
+		return err;
+	});
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {
+	return knex.schema.dropTable('notes');
+}
