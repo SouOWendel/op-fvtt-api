4e37f752247b29be9fdc24d7d612aa78be1be6a7
Wendel Henrique (SouOWendel) <souowendel@gmail.com>
2024-02-15 18:07:58 -0300

    build(delete files): product controller and route was deleted
    These files are no longer needed and also no longer make sense

--- expressAPI/src/routes/product.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable new-cap */
-
-import express from 'express';
-import {findAll} from '../controllers/product.js';
-const router = express.Router();
-
-router.get('/', findAll);
-
-export default router;
