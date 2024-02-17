f377b2fc35f0471aca5f41497b5d623361dfc161
Wendel Henrique (SouOWendel) <souowendel@gmail.com>
2024-02-16 21:48:19 -0300

    feat(server listener): console log with localhost + port has been added

--- src/server.js
+++ src/server.js
@@ -9,4 +9,5 @@ const PORT = process.env.PORT || 3333;
 app.listen(PORT, () => {
 	// mongoose.connect(process.env.MONGOOSE);
 	console.log('The app has been listening on port', PORT);
+	console.log(`Access on http://localhost:${PORT}`);
 });
