aee0efa796f321f71629099d5af67d5d370e600b
Wendel Henrique (SouOWendel) <souowendel@gmail.com>
2024-02-21 17:39:13 -0300

    feat(auth controller): auth verification with access token through cookies and renewToken function
    when the token expires, the renewToken function checks and renews the token and receives a cookie
    with it

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -21,24 +21,44 @@ const authenticate = async (request, response) => {
 
 		return response.status(200).json({ login: true });
 	} catch (error) {
-		console.log(error);
 		return response
 			.status(error.status || 500)
 			.json({ message: error.message || 'Internal Error' });
 	}
 };
 
-const isAuthenticated = async (request, response) => {
+const isAuthenticated = async (request, response, next) => {
 	try {
-		const headerToken = request.headers['authorization'];
-		if (!headerToken) return response.json('É preciso de um token de acesso!');
-		const token = await tokenCheck(headerToken);
-		return response.status(200).json({ login: true, token });
+		const accessToken = request.cookies.accessToken;
+		if (!accessToken && renewToken(request, response)) next();
+		else {
+			const decodedUsername = tokenCheck(accessToken, 'access');
+			request.username = decodedUsername;
+			next();
+		}
+		return response.status(200).json({ login: true });
 	} catch (error) {
 		return response
 			.status(error.status || 500)
 			.json({ message: error.message || 'Internal Error' });
 	}
+};
+
+const renewToken = async (request, response) => {
+	const refreshToken = request.cookies.refreshToken;
+	let exist = false;
+	if (!refreshToken) {
+		return res.json({ valid: false, message: 'No Refresh token' });
+	} else {
+		if (tokenCheck(refreshToken, 'refresh')) {
+			const accessToken = jwt.sign({ username }, process.env.accessToken, {
+				expiresIn: '1m',
+			});
+			response.cookie('accessToken', accessToken, { maxAge: 60000 });
+			exist = true;
+		}
+	}
+	return exist;
 };
 
 export { authenticate, isAuthenticated };
