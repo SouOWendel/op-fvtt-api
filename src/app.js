bbfe780cb86187a897b3263181581391e8d47af0
Wendel Henrique (SouOWendel) <souowendel@gmail.com>
2024-02-22 02:49:56 -0300

    feat(cors): `optionSuccessStatus` and `origin` was added on cors configuration

--- src/app.js
+++ src/app.js
@@ -9,8 +9,13 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(
 	cors({
-		origin: ['http://localhost:5173'],
-		credentials: true,
+		origin: [
+			'http://localhost:4173',
+			'http://localhost:3000',
+			'http://localhost:5173',
+		],
+		credentials: true, // access-control-allow-credentials:true
+		optionSuccessStatus: 200,
 	}),
 );
 app.use(router);
