48f7415057165fbc034774e4b10225408314a007
Wendel Henrique (SouOWendel) <souowendel@gmail.com>
2024-02-22 03:36:41 -0300

    feat(knex): production vars in knexfile

--- knexfile.js
+++ knexfile.js
@@ -18,6 +18,22 @@ export default {
 			max: Number(process.env.DATABASE_POOL_MAX),
 		},
 		migrations: {
-			directory: './expressAPI/src/database/migrations',
+			directory: './src/database/migrations',
 		},
-	}};
+	},
+	production: {
+		client: process.env.DATABASE_CLIENT,
+		connection: {
+			database: process.env.DATABASE_NAME,
+			user: process.env.DATABASE_USER_PROD,
+			password: process.env.DATABASE_PASSWORD_PROD,
+		},
+		pool: {
+			min: Number(process.env.DATABASE_POOL_MIN),
+			max: Number(process.env.DATABASE_POOL_MAX),
+		},
+		migrations: {
+			directory: './src/database/migrations',
+		},
+	},
+};
