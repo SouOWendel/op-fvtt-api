cd9c4b6edd722e114fe6dce620b1f308fed667e5
Wendel Henrique (SouOWendel) <souowendel@gmail.com>
2024-02-22 02:37:27 -0300

    feat(auth route): added a function and response after the authentication check middleware

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -5,6 +5,9 @@ import { authenticate, isAuthenticated } from '../controllers/auth.js';
 const router = express.Router();
 
 router.post('/auth', authenticate);
-router.get('/check-auth', isAuthenticated);
+router.get('/check-auth', isAuthenticated, (request, response) => {
+	console.log(request.url);
+	return response.status(200).json({ valid: true });
+});
 
 export default router;
