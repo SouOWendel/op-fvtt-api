ce0f114d0ff8939ab47d2201ba8b3ecd6cac4962
Wendel Henrique (SouOWendel) <souowendel@gmail.com>
2024-02-22 21:22:44 -0300

    feat(user route): authentication middleware has been added and create user route now need a secret

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,12 +1,17 @@
 import express from 'express';
 import * as userController from '../controllers/user.js';
+import { isAuthenticated } from '../controllers/auth.js';
+// import * as createError from 'http-errors';
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
-router.get('/', userController.findAll);
-router.post('/', userController.create);
-router.put('/', userController.update);
-router.delete('/:username', userController.remove);
+router.get('/', isAuthenticated, userController.findAll);
+router.post('/:secret', userController.create);
+router.put('/', isAuthenticated, userController.update);
+router.delete('/:username', isAuthenticated, userController.remove);
+
+// TODO: Error handling
+// router.use((error, request, response, next) => {});
 
 export default router;
