e25d436e46a78bceaa8b3d18c4f25709ea7c4d5c
Wendel Henrique (SouOWendel) <souowendel@gmail.com>
2024-02-26 16:41:12 -0300

    build(knex and knexfile): path of knexfile and env var has been changed

--- src/database/connections/knex.js
+++ src/database/connections/knex.js
@@ -1,9 +1,9 @@
 import knex from 'knex';
-import config from './knexfile.js';
+import config from '../../../knexfile.js';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const connection = knex(config[process.env.ENVIRONMENT]);
+const connection = knex(config[process.env.NODE_ENV]);
 
 export default connection;
