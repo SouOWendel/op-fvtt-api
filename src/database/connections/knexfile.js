0c019ed100f2b043dff40999a0fcead80ffe0ac9
Wendel Henrique (SouOWendel) <souowendel@gmail.com>
2024-02-26 13:33:36 -0300

    feat(knexfile): changed the production configuration and use .env for fields

---
+++ src/database/connections/knexfile.js
@@ -0,0 +1,40 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+export default {
+	development: {
+		client: process.env.DATABASE_CLIENT,
+		connection: {
+			database: process.env.DATABASE_NAME,
+			user: process.env.DATABASE_USER,
+			password: process.env.DATABASE_PASSWORD,
+		},
+		pool: {
+			min: Number(process.env.DATABASE_POOL_MIN),
+			max: Number(process.env.DATABASE_POOL_MAX),
+		},
+		migrations: {
+			directory: './src/database/migrations',
+		},
+	},
+	production: {
+		client: process.env.DATABASE_CLIENT,
+		connection: {
+			filename: process.env.FILENAME,
+			// database: process.env.DATABASE_NAME,
+			// user: process.env.DATABASE_USER_PROD,
+			// password: process.env.DATABASE_PASSWORD_PROD,
+		},
+		// pool: {
+		// 	min: Number(process.env.DATABASE_POOL_MIN),
+		// 	max: Number(process.env.DATABASE_POOL_MAX),
+		// },
+		migrations: {
+			directory: '../migrations',
+		},
+	},
+};
